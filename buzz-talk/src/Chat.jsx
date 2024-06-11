266e3901ab1b3edb039c46863a44f150d86d354f
sahan542 <sahanrashmika542@gmail.com>
2024-06-12 03:15:32 +0530

    added auto scrolling to newly send messages

--- buzz-talk/src/Chat.jsx
+++ buzz-talk/src/Chat.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
@@ -11,6 +11,7 @@ export default function Chat(){
     const [newMessageText, setNewMessageText] = useState('');
     const [messages, setMessages] = useState([]);
     const {username,id} = useContext(UserContext);
+    const divUnderMessages = useRef();
     useEffect(() =>{
         const ws = new WebSocket('ws://localhost:4040');
        setWs(ws);
@@ -55,6 +56,14 @@ export default function Chat(){
             }]));
     }
 
+    useEffect(() => {
+        const div = divUnderMessages.current;
+        if(div){
+            div.scrollIntoView({behavior:'smooth', block:'end'});
+        }
+
+    }, [messages]);
+
     const onlinePeopleExclOurUser = {...onlinePeople};
     delete onlinePeopleExclOurUser[id]; // Corrected line
    // console.log({onlinePeopleExclOurUser});
@@ -90,19 +99,20 @@ export default function Chat(){
                         </div>
                     )}
                     {!!selectedUserId && (
-                        <div className="relative h-full" >
-                            <div className="overflow-y-scroll absolute inset-0">
-                                {messagesWithoutDupes.map(message => (
-                                    <div className={(message.sender === id ? 'text-right' : 'text-left')} key={id}>
-                                        <div className={"text-left inline-block p-2 my-2 rounded-lg text-sm " +(message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
-                                        sender:{message.sender} <br/>
-                                        my id: {id} <br/>
-                                        {message.text}
+                            <div className="relative h-full mb-4" >
+                                <div className="overflow-y-scroll absolute top-0 right-0 left-0 bottom-2">
+                                    {messagesWithoutDupes.map(message => (
+                                        <div className={(message.sender === id ? 'text-right' : 'text-left')} key={id}>
+                                            <div className={"text-left inline-block p-2 my-2 rounded-lg text-sm " +(message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
+                                            sender:{message.sender} <br/>
+                                            my id: {id} <br/>
+                                            {message.text}
+                                            </div>
                                         </div>
-                                    </div>
-                                ))}
+                                    ))}
+                                    <div className="h-12" ref={divUnderMessages}></div>
+                                </div>
                             </div>
-                        </div>
                     )}
                 </div>
                 
